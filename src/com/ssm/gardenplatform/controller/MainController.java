d4b1184afa20b4bbc0f4858b36c480e3bd8eac7c
PJY <pooingx2@gmail.com>
2014-09-19 23:50:30 +0900

    start

--- src/com/ssm/gardenplatform/controller/MainController.java
+++ src/com/ssm/gardenplatform/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.ssm.gardenplatform.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,25 +16,42 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 	
-	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
 	public ModelAndView getMain(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		
-		System.out.println("\nmain");
+		System.out.println("\ntest");
+		printLog(request);
 		
-		ModelAndView mav = new ModelAndView("main");
+		ModelAndView mav = new ModelAndView("test");
 		mav.addObject("msg", "Garden Platform");
 		return mav;
 	}
 	
-	@RequestMapping(value = "/sample.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
 	public ModelAndView getSample(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		
-		System.out.println("\nsample");
+		System.out.println("\nmain");
+		printLog(request);
 		
-		ModelAndView mav = new ModelAndView("sample");
-		mav.addObject("msg", "Garden Platform");
-		
+		ModelAndView mav = new ModelAndView("main");
 		return mav;
+	}
+	
+	public void printLog(HttpServletRequest request) {
+		
+		Date dt = new Date();
+		System.out.println(dt.toString());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd, hh:mm:ss a"); 
+		System.out.println(sdf.format(dt).toString()); 
+		
+		String clientIp = request.getHeader("HTTP_X_FORWARDED_FOR");
+		if(null == clientIp || clientIp.length() == 0 || clientIp.toLowerCase().equals("unknown"))
+			clientIp = request.getHeader("REMOTE_ADDR");
+		 
+		if(null == clientIp || clientIp.length() == 0  || clientIp.toLowerCase().equals("unknown"))
+			clientIp = request.getRemoteAddr();
+		System.out.println(clientIp);
+		
 	}
 	
 }
