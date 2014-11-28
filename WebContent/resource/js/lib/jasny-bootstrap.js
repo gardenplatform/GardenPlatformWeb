d532ad3840d5030df63c6221e820bbb2c02493c2
Park ho <ds5ihy@naver.com>
2014-11-28 23:22:09 +0900

    test

--- WebContent/resource/js/lib/jasny-bootstrap.js
+++ WebContent/resource/js/lib/jasny-bootstrap.js
@@ -46,10 +46,10 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
   $.fn.emulateTransitionEnd = function (duration) {
     var called = false, $el = this
     $(this).one($.support.transition.end, function () { called = true })
-    var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
+    var callback = function () { if (!called) $($el).trigger($.support.transition.end) };
     setTimeout(callback, duration)
     return this
-  }
+  };
 
   $(function () {
     $.support.transition = transitionEnd()
